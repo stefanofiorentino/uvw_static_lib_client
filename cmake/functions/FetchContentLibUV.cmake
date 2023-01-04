6c1359c8eb953145e42adead00e70b5649d4b9c0
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2023-01-04 14:53:43 +0100

    update libuv to v1.44.2 (and uvw)

--- cmake/functions/FetchContentLibUV.cmake
+++ cmake/functions/FetchContentLibUV.cmake
@@ -1,15 +1,18 @@
 function(fetch_content_libuv)
-    FetchContent_Declare(
-            libuv
-            GIT_REPOSITORY https://github.com/libuv/libuv.git
-            GIT_TAG v1.37.0
-            GIT_SHALLOW 1
-    )
+    option(FETCH_LIBUV "Fetch the libuv repo using CMake FetchContent facility" ON)
+    if (FETCH_LIBUV)
+        FetchContent_Declare(
+                libuv
+                GIT_REPOSITORY https://github.com/libuv/libuv.git
+                GIT_TAG v1.44.2
+                GIT_SHALLOW 1
+        )
 
-    FetchContent_GetProperties(libuv)
+        FetchContent_GetProperties(libuv)
 
-    if(NOT libuv_POPULATED)
-        FetchContent_Populate(libuv)
-        add_subdirectory(${libuv_SOURCE_DIR} ${libuv_BINARY_DIR})
+        if(NOT libuv_POPULATED)
+            FetchContent_Populate(libuv)
+            add_subdirectory(${libuv_SOURCE_DIR} ${libuv_BINARY_DIR})
+        endif()
     endif()
 endfunction()
--- cmake/functions/FetchContentLibUVW.cmake
+++ cmake/functions/FetchContentLibUVW.cmake
@@ -1,23 +1,27 @@
 function(fetch_content_uvw)
-    set(OLD_BUILD_TESTING ${BUILD_TESTING})
-    set(BUILD_TESTING OFF CACHE INTERNAL "")  # Forces the value
-    set(libuv_buildtests OFF CACHE INTERNAL "")  # Forces the value
-    set(BUILD_UVW_LIBS ON CACHE INTERNAL "")  # Forces the value
+    option(FETCH_UVW "Fetch the uvw repo using CMake FetchContent facility" ON)
 
-    FetchContent_Declare(
-            uvw
+    if (FETCH_UVW)
+        set(OLD_BUILD_TESTING ${BUILD_TESTING})
+        set(BUILD_TESTING OFF CACHE INTERNAL "")  # Forces the value
+        set(libuv_buildtests OFF CACHE INTERNAL "")  # Forces the value
+        set(BUILD_UVW_LIBS ON CACHE INTERNAL "")  # Forces the value
 
-            GIT_REPOSITORY https://github.com/stefanofiorentino/uvw.git
-            GIT_TAG experimental
-            GIT_SHALLOW 1
-    )
+        FetchContent_Declare(
+                uvw
 
-    FetchContent_GetProperties(uvw)
+                GIT_REPOSITORY https://github.com/skypjack/uvw.git
+                GIT_TAG 3db9e8f75a4351325e1ea00e17586af2b00cf1ea
+                GIT_SHALLOW 1
+        )
 
-    if(NOT uvw_POPULATED)
-        FetchContent_Populate(uvw)
-        add_subdirectory(${uvw_SOURCE_DIR} ${uvw_BINARY_DIR} EXCLUDE_FROM_ALL)
+        FetchContent_GetProperties(uvw)
+
+        if(NOT uvw_POPULATED)
+            FetchContent_Populate(uvw)
+            add_subdirectory(${uvw_SOURCE_DIR} ${uvw_BINARY_DIR} EXCLUDE_FROM_ALL)
+        endif()
+
+        set(BUILD_TESTING ${OLD_BUILD_TESTING} CACHE INTERNAL "")
     endif()
-
-    set(BUILD_TESTING ${OLD_BUILD_TESTING} CACHE INTERNAL "")
 endfunction()
